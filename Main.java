4e7a3f54a59a801f30ed676194e808768177966e
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 07:32:03 +0900

    クラスフィールドを使ってインスタンスのせフィールドを使ってインスタンスの生成回数使ってインスタンスの生成回数を数えインスタンスの生成回数を数える

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 class Main {
   public static void main(String[] args) {
     Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    person1.printData();
 
-    System.out.println(person1.firstName);
-    System.out.println(person1.lastName);
-    System.out.println(person1.age);
-    System.out.println(person1.height);
-    System.out.println(person1.weight);
+    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
+    person2.printData();
+
+    // 「合計◯◯人です」と出力してください
+    System.out.println("合計" + Person.count + "人です");
   }
 }
--- Person.java
+++ Person.java
@@ -1,11 +1,15 @@
 class Person {
+  // int型のクラスフィールドcountを定義し、0を代入してください
+  public static int count = 0;
   public String firstName;
   public String lastName;
   public int age;
-  public double height;
-  public double weight;
+  public double height, weight;
 
   Person(String firstName, String lastName, int age, double height, double weight) {
+    // 変数countに1を足してください
+    Person.count++;
+
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
@@ -21,10 +25,16 @@ class Person {
     return this.weight / this.height / this.height;
   }
 
-  // printDataメソッドを定義してください
   public void printData() {
     System.out.println("私の名前は" + this.fullName() + "です");
     System.out.println("年齢は" + this.age + "歳です");
     System.out.println("BMIは" + Math.round(this.bmi()) + "です");
   }
 }
+
+// クラスフィールド [START] ========================================
+// 何人分のインスタンスを生成したか数えるために、countというフィールドで、インスタンスの生成回数を保存するようにしてみます。countは個々のインスタンスが持つものではなく、Personクラスが持っていればよいので、クラスフィールドにしてあげましょう。
+//フィールドに初期値を設定する
+// フィールドには、定義時に初期値を設定しておくことができます。
+// countはインスタンスの生成回数なので、下図のように定義時に0を代入しておきましょう。
+// クラスフィールド [END] ==========================================
