cc7fdfdf610e9fe54764f7bb13a2a835894cb1af
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 07:19:22 +0900

    インスタンスメソッドフルネームとBMIを定義

--- Person.java
+++ Person.java
@@ -1,17 +1,25 @@
 class Person {
-  // 以下にインスタンスフィールドを定義してください
   public String firstName;
   public String lastName;
   public int age;
   public double height;
   public double weight;
 
-  // 以下にコンストラクタを定義し、インスタンスフィールドに値をセットしてください
   Person(String firstName, String lastName, int age, double height, double weight) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.height = height;
     this.weight = weight;
+  }
+
+  // fullNameメソッドを定義してください
+  public String fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  // bmiメソッドを定義してください
+  public double bmi() {
+    return this.weight / this.height / this.height;
   }
 }
