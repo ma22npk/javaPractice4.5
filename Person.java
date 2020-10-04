7e9d406d09b22c68497772b2a774d3c8630ff67c
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 06:37:45 +0900

    フィールドとコンストラクタの定義

---
+++ Person.java
@@ -0,0 +1,17 @@
+class Person {
+  // 以下にインスタンスフィールドを定義してください
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
+
+  // 以下にコンストラクタを定義し、インスタンスフィールドに値をセットしてください
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+}
